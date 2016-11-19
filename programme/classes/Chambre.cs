4dbf2317096901a823cc72e3873afe7ca2db7c35
RValeye <romain.valeye@gmail.com>
2016-11-19 21:02:11 +0100

    Modifications des classes ...
    Suite à une modification dans la base de données, j'ai mis à jour en
    conséquence les classes
    Les tables "Lit" et "contenir" ne sont plus utiles, une table
    "TypeChambre" remplace celles-ci.

--- programme/classes/Chambre.cs
+++ programme/classes/Chambre.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace PPE_ABAS
 {
-    class Chambre
+    public class Chambre
     {
         // Etage_Hotel_hotel_bat_id
         public int Etage_Hotel_hotel_bat_id { get; set; }
@@ -13,5 +13,7 @@ namespace PPE_ABAS
         public int Etage_etage_id { get; set; }
         // ch_id
         public int ch_id { get; set; }
+        // ch_type_id
+        public int ch_type_id { get; set; }
     }
 }
